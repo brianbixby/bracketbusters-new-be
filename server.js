86a95477a0f4a83c6da992afd8c523a1b2d8b8c4
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-06-21 12:26:40 -0700

    updating cors

--- server.js
+++ server.js
@@ -2,16 +2,28 @@
 
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const db = require('./config/connection');
 const routes = require('./routes/allRoutes');
-const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+
+const whitelist = [process.env.WHITELIST, process.env.WHITELIST1, process.env.WHITELIST2];
+app.use(cors({
+    credentials: true,
+    origin: (origin, cb) => {
+        if (whitelist.indexOf(origin) != -1 || origin === undefined) {
+            cb(null, true);
+        } else {
+            cb(new Error(`${origin} Not allowed by CORS`));
+        }
+    },
+}));
+
 app.use(routes);
 
 db.once('open', (err, resp) => {
